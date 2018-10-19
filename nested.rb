003882512bf1e729f3807f65c9222abd41325439
Jane Venter <janeanneventer@gmail.com>
2018-10-19 18:44:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-  
+    programmer_hash[:grace_hopper]
     #programer_hash[:grace_hopper] = {:known_for => "COBOL, :languages => ["COBOL", "FORTRAN"]}
 
 end
